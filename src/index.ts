5651be124a11116f085312d4d0353f80a5281095
biaska <biaska70@gmail.com>
2025-09-23 22:44:29 -0700

    feat(validate): on error throw ZodError

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /// <reference types="./express.d.ts" />
-import type { NextFunction, Request, RequestHandler, Response } from 'express';
-import express from 'express';
-import { type ZodError, type ZodRawShape, type ZodType, z } from 'zod';
+import type { NextFunction, Request, RequestHandler, Response } from "express";
+import express from "express";
+import { ZodError, type ZodRawShape, type ZodType, z } from "zod";
 
-const types = ['query', 'params', 'body'] as const;
+const types = ["query", "params", "body"] as const;
 
 /**
  * A ZodType type guard.
@@ -11,30 +11,30 @@ const types = ['query', 'params', 'body'] as const;
  * @returns Whether the provided schema is a ZodType.
  */
 function isZodType(schema: unknown): schema is ZodType {
-	return !!schema && typeof (schema as ZodType).safeParseAsync === 'function';
+  return !!schema && typeof (schema as ZodType).safeParseAsync === "function";
 }
 
 // Override express@^5 request.query getter to provider setter
-const descriptor = Object.getOwnPropertyDescriptor(express.request, 'query');
+const descriptor = Object.getOwnPropertyDescriptor(express.request, "query");
 if (descriptor) {
-	Object.defineProperty(express.request, 'query', {
-		get(this: Request) {
-			if (Object.hasOwn(this, '_query')) return this._query;
-			return descriptor?.get?.call(this);
-		},
-		set(this: Request, query: unknown) {
-			this._query = query;
-		},
-		configurable: true,
-		enumerable: true
-	});
+  Object.defineProperty(express.request, "query", {
+    get(this: Request) {
+      if (Object.hasOwn(this, "_query")) return this._query;
+      return descriptor?.get?.call(this);
+    },
+    set(this: Request, query: unknown) {
+      this._query = query;
+    },
+    configurable: true,
+    enumerable: true,
+  });
 }
 
 /**
  * Generates a middleware function for Express.js that validates request params, query, and body.
  * This function uses Zod schemas to perform validation against the provided schema definitions.
  *
- * @param schemas - An object containing Zod schemas for params, query, and body.  Optional handler for custom error handling.
+ * @param schemas - An object containing Zod schemas for params, query, and body.
  * @returns An Express.js middleware function that validates the request based on the provided schemas.
  *          It attaches validated data to the request object and sends error details if validation fails.
  * @template TParams - Type definition for params schema.
@@ -69,36 +69,51 @@ if (descriptor) {
  *
  * app.listen(3000, () => console.log('Server running on port 3000'));
  */
-export default function validate<TParams extends ValidationSchema, TQuery extends ValidationSchema, TBody extends ValidationSchema>(
-	schemas: CompleteValidationSchema<TParams, TQuery, TBody>
-): RequestHandler<ZodOutput<TParams>, any, ZodOutput<TBody>, ZodOutput<TQuery>> {
-	// Create validation objects for each type
-	const validation = {
-		params: isZodType(schemas.params) ? schemas.params : z.strictObject(schemas.params ?? {}),
-		query: isZodType(schemas.query) ? schemas.query : z.strictObject(schemas.query ?? {}),
-		body: isZodType(schemas.body) ? schemas.body : z.strictObject(schemas.body ?? {})
-	};
+export default function validate<
+  TParams extends ValidationSchema,
+  TQuery extends ValidationSchema,
+  TBody extends ValidationSchema
+>(
+  schemas: CompleteValidationSchema<TParams, TQuery, TBody>
+): RequestHandler<
+  ZodOutput<TParams>,
+  any,
+  ZodOutput<TBody>,
+  ZodOutput<TQuery>
+> {
+  // Create validation objects for each type
+  const validation = {
+    params: isZodType(schemas.params)
+      ? schemas.params
+      : z.strictObject(schemas.params ?? {}),
+    query: isZodType(schemas.query)
+      ? schemas.query
+      : z.strictObject(schemas.query ?? {}),
+    body: isZodType(schemas.body)
+      ? schemas.body
+      : z.strictObject(schemas.body ?? {}),
+  };
 
-	return async (req, res, next): Promise<void> => {
-		const errors: ErrorListItem[] = [];
+  return async (req, res, next): Promise<void> => {
+    const errors: ErrorListItem[] = [];
+    const issues = [];
 
-		// Validate all types (params, query, body)
-		for (const type of types) {
-			const parsed = await validation[type].safeParseAsync(req[type] ?? {});
-			if (parsed.success) req[type] = parsed.data as any;
-			else errors.push({ type, errors: parsed.error });
-		}
+    // Validate all types (params, query, body)
+    for (const type of types) {
+      const parsed = await validation[type].safeParseAsync(req[type] ?? {});
+      if (parsed.success) req[type] = parsed.data as any;
+      else {
+        issues.push(...parsed.error.issues);
+      }
+    }
 
-		// Return all errors if there are any
-		if (errors.length > 0) {
-			// If a custom error handler is provided, use it
-			if (schemas.handler) return schemas.handler(errors, req, res, next);
-			res.status(400).send(errors.map(error => ({ type: error.type, errors: error.errors.issues })));
-			return;
-		}
+    // Return all errors if there are any
+    if (issues.length > 0) {
+      throw new ZodError(issues);
+    }
 
-		return next();
-	};
+    return next();
+  };
 }
 
 /**
@@ -111,27 +126,11 @@ type DataType = (typeof types)[number];
  * and the associated ZodError.
  */
 export interface ErrorListItem {
-	type: DataType;
-	errors: ZodError;
+  type: DataType;
+  errors: ZodError;
 }
 
 export type Unvalidated = unknown;
-
-/**
- * Represents an Express.js error request handler where the params, query and body are of unknown type as validation failed.
- */
-export type ErrorRequestHandler<
-	P = Unvalidated,
-	ResBody = any,
-	ReqBody = Unvalidated,
-	ReqQuery = unknown,
-	LocalsObj extends Record<string, any> = Record<string, any>
-> = (
-	err: ErrorListItem[],
-	req: Request<P, ResBody, ReqBody, ReqQuery, LocalsObj>,
-	res: Response<ResBody, LocalsObj>,
-	next: NextFunction
-) => void | Promise<void>;
 
 /**
  * Represents a generic type for route validation, which can be applied to params, query, or body.
@@ -142,21 +141,20 @@ export type ValidationSchema = ZodType | ZodRawShape;
 /**
  * Defines the structure for the schemas provided to the validate middleware.
  * Each property corresponds to a different part of the request (params, query, body)
- * and should be a record of Zod types for validation. Optional handler for custom error handling.
+ * and should be a record of Zod types for validation.
  *
  * @template TParams - Type definition for params schema.
  * @template TQuery - Type definition for query schema.
  * @template TBody - Type definition for body schema.
  */
 export interface CompleteValidationSchema<
-	TParams extends ValidationSchema = ValidationSchema,
-	TQuery extends ValidationSchema = ValidationSchema,
-	TBody extends ValidationSchema = ValidationSchema
+  TParams extends ValidationSchema = ValidationSchema,
+  TQuery extends ValidationSchema = ValidationSchema,
+  TBody extends ValidationSchema = ValidationSchema
 > {
-	handler?: ErrorRequestHandler;
-	params?: TParams;
-	query?: TQuery;
-	body?: TBody;
+  params?: TParams;
+  query?: TQuery;
+  body?: TBody;
 }
 
 /**
@@ -166,14 +164,20 @@ export interface CompleteValidationSchema<
  *
  * @template T - The validation type (params, query, or body).
  */
-export type ZodOutput<T extends ValidationSchema | undefined> = T extends ValidationSchema
-	? z.output<T extends ZodRawShape ? z.ZodObject<T> : T>
-	: Unvalidated;
+export type ZodOutput<T extends ValidationSchema | undefined> =
+  T extends ValidationSchema
+    ? z.output<T extends ZodRawShape ? z.ZodObject<T> : T>
+    : Unvalidated;
 
 /**
  * A utility type to ensure other middleware types don't conflict with the validate middleware.
  */
-export type WeakRequestHandler = RequestHandler<Unvalidated, Unvalidated, Unvalidated, Unvalidated>;
+export type WeakRequestHandler = RequestHandler<
+  Unvalidated,
+  Unvalidated,
+  Unvalidated,
+  Unvalidated
+>;
 
 /**
  * A utility type to ensure the Request is typed correctly.
@@ -206,8 +210,8 @@ export type WeakRequestHandler = RequestHandler<Unvalidated, Unvalidated, Unvali
  * app.post('/handler/:id', validate(schema), requestHandler);
  */
 export type ValidatedRequest<T extends CompleteValidationSchema> = Request<
-	ZodOutput<T['params']>,
-	any,
-	ZodOutput<T['body']>,
-	ZodOutput<T['query']>
+  ZodOutput<T["params"]>,
+  any,
+  ZodOutput<T["body"]>,
+  ZodOutput<T["query"]>
 >;
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,15 @@
-import express, { type NextFunction, type Request, type Response } from 'express';
-import { z } from 'zod';
-import validate, { type CompleteValidationSchema, type ValidatedRequest, type WeakRequestHandler } from '../src/index';
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+  type ErrorRequestHandler,
+} from "express";
+import { z } from "zod";
+import validate, {
+  type CompleteValidationSchema,
+  type ValidatedRequest,
+  type WeakRequestHandler,
+} from "../src/index";
 
 const app = express();
 const port = 3000;
@@ -8,51 +17,60 @@ const port = 3000;
 app.use(express.json());
 
 const authenticate = (_req: Request, _res: Response, next: NextFunction) => {
-	next();
+  next();
 };
 
 const schema = {
-	query: {
-		name: z.string().min(3).max(10),
-		age: z.coerce.number().min(18)
-	},
-	body: {
-		title: z.string().max(4)
-	},
-	params: {
-		id: z.coerce.number()
-	}
+  query: {
+    name: z.string().min(3).max(10),
+    age: z.coerce.number().min(18),
+  },
+  body: {
+    title: z.string().max(4),
+  },
+  params: {
+    id: z.coerce.number(),
+  },
 } satisfies CompleteValidationSchema;
 
 app.post(
-	'/:id',
-	authenticate as WeakRequestHandler,
-	validate({
-		handler: (errors, _req, res) => {
-			console.error(errors);
-			res.status(400).send('Validation failed');
-			return;
-		},
-		...schema
-	}),
-	(req, res) => {
-		const { name, age } = req.query;
-		const { id } = req.params;
-		// @ts-expect-error
-		const { title, notFound } = req.body;
+  "/:id",
+  authenticate as WeakRequestHandler,
+  validate(schema),
+  (req, res) => {
+    console.log("Trying to handle request...");
+    const { name, age } = req.query;
+    const { id } = req.params;
+    // @ts-expect-error
+    const { title, notFound } = req.body;
 
-		res.send(`Hello ${title} ${name}! (Your age is ${age} and your ID is ${id})`);
-	}
+    res.send(
+      `Hello ${title} ${name}! (Your age is ${age} and your ID is ${id})`
+    );
+  }
 );
 
-const requestHandler = (req: ValidatedRequest<typeof schema>, res: Response) => {
-	const { name, age } = req.query;
-	const { id } = req.params;
-	const { title } = req.body;
+const requestHandler = (
+  req: ValidatedRequest<typeof schema>,
+  res: Response
+) => {
+  const { name, age } = req.query;
+  const { id } = req.params;
+  const { title } = req.body;
 
-	res.send(`Hello ${title} ${name}! (Your age is ${age} and your ID is ${id})`);
+  res.send(`Hello ${title} ${name}! (Your age is ${age} and your ID is ${id})`);
 };
 
-app.post('/handler/:id', validate(schema), requestHandler);
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  const error = err as z.ZodError;
+  console.log("Caught by global error handler");
+  console.log("Errors", error.issues);
+  res.status(400).send("Validation failed");
+  return;
+};
+
+app.post("/handler/:id", validate(schema), requestHandler);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
